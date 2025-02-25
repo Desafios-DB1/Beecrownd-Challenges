b9d6d2a56cb1da772e800057e9ed974061b599ae
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-25 10:08:46 -0300

    Create /api/hello get endpoint

---
+++ 1000 - Hello World/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1000___Hello_World.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello World";
+    }
+}
