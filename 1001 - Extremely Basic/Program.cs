084cc89c71a8cae202cb18e6303fc0fe5058b2e5
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-25 13:24:01 -0300

    registering sumService in Program.cs

--- 1001 - Extremely Basic/Program.cs
+++ 1001 - Extremely Basic/Program.cs
@@ -1,5 +1,12 @@
+using _1001___Extremely_Basic.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<SumService>();
 builder.Services.AddControllers();
+
 var app = builder.Build();
+
 app.MapControllers();
+
 app.Run();
