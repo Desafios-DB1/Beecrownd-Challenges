7dc25716dd02b1cc15ebe56ac0f59ddb5b3e0746
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-25 13:41:46 -0300

    added 1000 - HelloControllerTests.cs

---
+++ 1000 - HelloWorld.Tests/HelloControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using _1000___Hello_World.Controllers;
+
+namespace _1000___HelloWorld.Tests;
+
+public class HelloControllerTests
+{
+    [Fact]
+    public void Get_ReturnsHelloWorld()
+    {
+        var controller = new HelloController();
+        
+        var result = controller.Get();
+        
+        Assert.Equal("Hello World", result);
+    }
+}
