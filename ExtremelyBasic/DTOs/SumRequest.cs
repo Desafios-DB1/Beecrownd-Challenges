fc009942bcd2cbd4364db6483d800fbfcdab7279
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-26 08:54:56 -0300

    adicionado nullable nos ints de SumRequest.cs

--- ExtremelyBasic/DTOs/SumRequest.cs
+++ ExtremelyBasic/DTOs/SumRequest.cs
@@ -2,6 +2,6 @@
 
 public class SumRequest
 {
-    public int Number1 { get; set; }
-    public int Number2 { get; set; }
+    public int? Number1 { get; set; }
+    public int? Number2 { get; set; }
 }
