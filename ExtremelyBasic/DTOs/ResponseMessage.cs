7336a400c11fd8a5ba7a6334988198a02847e8a0
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-26 08:28:17 -0300

    criado ResponseMessage.cs nas DTOs para garantir a tipagem da mensagem de resposta

---
+++ ExtremelyBasic/DTOs/ResponseMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace _1001___Extremely_Basic.DTOs;
+
+public class ResponseMessage
+{
+    public string Message { get; set; }
+
+    public ResponseMessage(string message)
+    {
+        Message = message;
+    }
+}
