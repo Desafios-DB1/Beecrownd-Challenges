db6a059467a9f0407dd03240415d8be23c2ca8de
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-26 11:02:51 -0300

    criado uma lista temporária para armazenar os competidores

---
+++ IdentificandoCha/Services/ContestantService.cs
@@ -0,0 +1,28 @@
+﻿using IdentificandoCha.DTOs;
+
+namespace IdentificandoCha.Services;
+
+public class ContestantService
+{
+    private static List<ContestantData> _contestants = new List<ContestantData>();
+    private static int _currentId = 0;
+
+    public ContestantData AddContestant(ContestantData contestant)
+    {
+        contestant.Id= ++_currentId;
+        
+        _contestants.Add(contestant);
+        
+        return contestant;
+    }
+
+    public List<ContestantData> GetAllContestants()
+    {
+        return _contestants.ToList();
+    }
+
+    public ContestantData GetContestantById(int id)
+    {
+        return _contestants.FirstOrDefault(x => x.Id == id);
+    }
+}
