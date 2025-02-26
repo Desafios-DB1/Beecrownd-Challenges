dc3139597b0c1a4d7d6f100ee1f609aa98d48081
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-26 11:01:14 -0300

    criado ContestantController.cs para mapear o Get de uma lista dos competidores cadastrados e o Post para cadastro dos competidores ambos no endpoint /contestant

---
+++ IdentificandoCha/Controllers/ContestantController.cs
@@ -0,0 +1,32 @@
+﻿using IdentificandoCha.DTOs;
+using IdentificandoCha.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentificandoCha.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContestantController : ControllerBase
+{
+    private readonly ContestantService _contestantService;
+
+    public ContestantController(ContestantService contestantService)
+    {
+        _contestantService = contestantService;
+    }
+    
+    [HttpPost]
+    public IActionResult RegisterContestants([FromBody] ContestantData contestant)
+    {
+        if (string.IsNullOrEmpty(contestant.Name)) {return BadRequest(new ResponseMessage("Forneça um nome para o competidor!"));}
+        var newContestant = _contestantService.AddContestant(contestant);
+        return CreatedAtAction(nameof(GetContestants), $"{newContestant.Name} foi registrado com sucesso! Seu número é {newContestant.Id}");
+    }
+
+    [HttpGet]
+    public IActionResult GetContestants()
+    {
+        var contestants = _contestantService.GetAllContestants();
+        return Ok(contestants);
+    }
+}
