c2b53d5098698d775ab8860e77045f6100cd1ed6
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-27 13:45:40 -0300

    alterado a constante _contestants para Contestants e colocada como readonly. alterado métodos AddContestant, GetAllContestants e AddPoints para static

--- IdentificandoCha/Services/ContestantService.cs
+++ IdentificandoCha/Services/ContestantService.cs
@@ -4,26 +4,26 @@ namespace IdentificandoCha.Services;
 
 public class ContestantService
 {
-    private static List<ContestantData> _contestants = new List<ContestantData>();
+    private static readonly List<ContestantData> Contestants = [];
     private static int _currentId = 0;
 
-    public ContestantData AddContestant(ContestantData contestant)
+    public static ContestantData AddContestant(ContestantData contestant)
     {
         contestant.Id= ++_currentId;
         
-        _contestants.Add(contestant);
+        Contestants.Add(contestant);
         
         return contestant;
     }
 
-    public List<ContestantData> GetAllContestants()
+    public static List<ContestantData>? GetAllContestants()
     {
-        return _contestants.ToList();
+        return Contestants.Count != 0 ? Contestants.ToList() : null;
     }
 
-    public void AddPoints(int contestantId, int points)
+    public static void AddPoints(int contestantId, int points)
     {
-        var contestant = _contestants.FirstOrDefault(c => c.Id == contestantId);
+        var contestant = Contestants.FirstOrDefault(c => c.Id == contestantId);
         if (contestant == null)
         {
             throw new Exception("Competidor não encontrado");
