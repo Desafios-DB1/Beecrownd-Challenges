a11ae3bc914ce6751e5d6affaf69e8015ac575f9
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-27 13:44:07 -0300

    Alterado o campo Name para obrigatório e init only

--- IdentificandoCha/DTOs/ContestantData.cs
+++ IdentificandoCha/DTOs/ContestantData.cs
@@ -3,6 +3,6 @@
 public class ContestantData
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; init; }
     public int Points { get; set; }
 }
