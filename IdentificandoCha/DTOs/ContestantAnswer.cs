7d90c57c71b133a067181ebdc301ce01f5274306
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-27 13:42:37 -0300

    Alterado os campos ContestantId e Answer para init only

--- IdentificandoCha/DTOs/ContestantAnswer.cs
+++ IdentificandoCha/DTOs/ContestantAnswer.cs
@@ -5,7 +5,7 @@ namespace IdentificandoCha.DTOs;
 public class ContestantAnswer
 {
     [Required]
-    public int ContestantId { get; set; }
+    public int ContestantId { get; init; }
     [Range(1, 5, ErrorMessage = "O valor deve estar entre 1 e 5")]
-    public int Answer { get; set; }
+    public int Answer { get; init; }
 }
