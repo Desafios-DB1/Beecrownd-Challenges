7888b6bb8ccc7772a049a0d41fddc60ddacbe38e
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:11:30 -0300

    feat: adicionado o mapeamento dos services como scoped, dos repositories e dos validators

--- IdentificandoCha/Program.cs
+++ IdentificandoCha/Program.cs
@@ -1,10 +1,25 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using IdentificandoCha.DTOs;
+using IdentificandoCha.Repository;
 using IdentificandoCha.Services;
+using IdentificandoCha.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ContestantService>();
-builder.Services.AddSingleton<ChallengeService>();
+
+builder.Services.AddScoped<ContestantService>();
+builder.Services.AddScoped<ChallengeService>();
+builder.Services.AddScoped<ScoringService>();
+
+builder.Services.AddScoped<ContestantRepository>();
+builder.Services.AddScoped<ChallengeRepository>();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddTransient<IValidator<ContestantData>, ContestantDataValidator>();
+builder.Services.AddTransient<IValidator<List<ContestantAnswer>>, ContestantAnswerValidator>();
 
 var app = builder.Build();
 
