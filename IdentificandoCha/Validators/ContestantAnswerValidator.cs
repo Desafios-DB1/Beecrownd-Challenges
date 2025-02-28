157e6f991c7bbe95f8e5c521b296f657dd29ed23
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 12:56:43 -0300

    feat: validator para as respostas

---
+++ IdentificandoCha/Validators/ContestantAnswerValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using IdentificandoCha.DTOs;
+using IdentificandoCha.Services;
+
+namespace IdentificandoCha.Validators;
+
+public class ContestantAnswerValidator : AbstractValidator<List<ContestantAnswer>>
+{
+    public ContestantAnswerValidator(ContestantService contestantService)
+    {
+        RuleFor(x => x)
+            .Must(answers => answers.Count == contestantService.GetAllContestants().Count)
+            .WithMessage("Todos os participantes devem enviar somente uma resposta!");
+    }
+}
