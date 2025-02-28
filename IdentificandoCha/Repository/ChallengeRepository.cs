c5e271e74fcd7e54984b1de9c8314b68005e4b31
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 12:54:16 -0300

    feat: criado ChallengeRepository.cs para manipulação dos dados do desafio

---
+++ IdentificandoCha/Repository/ChallengeRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace IdentificandoCha.Repository;
+
+public class ChallengeRepository
+{
+    private readonly Dictionary<int, int> _correctAnswers = new()
+    {
+        {1, 3},
+        {2, 4},
+        {3, 1},
+        {4, 2},
+        {5, 5}
+    };
+    
+    public int? GetCorrectAnswer(int challengeId) =>
+        _correctAnswers.TryGetValue(challengeId, out var correctAnswer) ? correctAnswer : null;
+}
