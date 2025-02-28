156522c8d32fa8909cd6c6c8dac929a5d90b453f
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:07:58 -0300

    feat: adicionado o repository para manipulação dos dados dos competidores

---
+++ IdentificandoCha/Repository/ContestantRepository.cs
@@ -0,0 +1,36 @@
+﻿using IdentificandoCha.DTOs;
+
+namespace IdentificandoCha.Repository;
+
+public class ContestantRepository
+{
+    private static readonly List<ContestantData> Contestants = [];
+    private static int _currentId = 0;
+    
+    public static ContestantData Add(ContestantData contestant)
+    {
+        contestant.Id= ++_currentId;
+        Contestants.Add(contestant);
+        return contestant;
+    }
+    
+    public static List<ContestantData> GetAll()
+    {
+        return Contestants.ToList();
+    }
+
+    public static ContestantData? GetById(int id)
+    {
+        return Contestants.FirstOrDefault(c => c.Id == id);
+    }
+
+    public static void AddPoints(ContestantData contestant, int points)
+    {
+        contestant.Points += points;
+    }
+
+    public static bool Exists(int id)
+    {
+        return Contestants.Any(c => c.Id == id);
+    }
+}
