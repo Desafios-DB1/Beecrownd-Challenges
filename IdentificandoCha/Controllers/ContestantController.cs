209c605066f84fb1da2edbb399ea71dfe144aad4
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:06:53 -0300

    refactor: movida a validação para o validator e adicionada a exception personalizada

--- IdentificandoCha/Controllers/ContestantController.cs
+++ IdentificandoCha/Controllers/ContestantController.cs
@@ -1,4 +1,6 @@
-﻿using IdentificandoCha.DTOs;
+﻿using FluentValidation;
+using IdentificandoCha.DTOs;
+using IdentificandoCha.Exceptions;
 using IdentificandoCha.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,20 +8,26 @@ namespace IdentificandoCha.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ContestantController() : ControllerBase
+public class ContestantController(IValidator<ContestantData> validator, ContestantService contestantService) : ControllerBase
 {
     [HttpPost]
     public IActionResult RegisterContestants([FromBody] ContestantData contestant)
     {
-        if (string.IsNullOrEmpty(contestant.Name)) {return BadRequest(new ResponseMessage("Forneça um nome para o competidor!"));}
-        var newContestant = ContestantService.AddContestant(contestant);
+        var validationResult = validator.Validate(contestant);
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            throw new BusinessException(errors);
+        }
+        
+        var newContestant = contestantService.AddContestant(contestant);
         return CreatedAtAction(nameof(GetContestants), $"{newContestant.Name} foi registrado com sucesso! Seu número é {newContestant.Id}");
     }
 
     [HttpGet]
     public IActionResult GetContestants()
     {
-        var contestants = ContestantService.GetAllContestants();
+        var contestants = contestantService.GetAllContestants();
         if (contestants?.Count == 0 || contestants == null)
         {
             return NoContent();
