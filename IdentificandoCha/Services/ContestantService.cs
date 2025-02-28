6b9d7edca46fde632c9147bfe034e592a87b7c36
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:09:56 -0300

    refactor: movida a manipulação de dados para o repository

--- IdentificandoCha/Services/ContestantService.cs
+++ IdentificandoCha/Services/ContestantService.cs
@@ -1,33 +1,25 @@
 ﻿using IdentificandoCha.DTOs;
+using IdentificandoCha.Repository;
 
 namespace IdentificandoCha.Services;
 
-public class ContestantService
+public class ContestantService ()
 {
-    private static readonly List<ContestantData> Contestants = [];
-    private static int _currentId = 0;
-
     public static ContestantData AddContestant(ContestantData contestant)
     {
-        contestant.Id= ++_currentId;
-        
-        Contestants.Add(contestant);
-        
-        return contestant;
+        return ContestantRepository.Add(contestant);
     }
-
-    public static List<ContestantData>? GetAllContestants()
+    
+    public static List<ContestantData> GetAllContestants()
     {
-        return Contestants.Count != 0 ? Contestants.ToList() : null;
+        return ContestantRepository.GetAll();
     }
 
     public static void AddPoints(int contestantId, int points)
     {
-        var contestant = Contestants.FirstOrDefault(c => c.Id == contestantId);
-        if (contestant == null)
-        {
-            throw new Exception("Competidor não encontrado");
-        }
-        contestant.Points += points;
+        if (!ContestantRepository.Exists(contestantId)) return;
+        
+        var contestant = ContestantRepository.GetById(contestantId);
+        ContestantRepository.AddPoints(contestant!, points);
     }
 }
