4b583cf6a578e25086055c1321d84de81595e26b
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 14:45:58 -0300

    feat: criado construtor implicito de AnswersRequest.cs

--- IdentificandoCha/DTOs/AnswersRequest.cs
+++ IdentificandoCha/DTOs/AnswersRequest.cs
@@ -2,6 +2,15 @@
 
 public class AnswersRequest
 {
-    public int ChallengeId { get; init; }
+    public int ChallengeId { get; private init; }
     public required List<ContestantAnswer> Answers { get; init; }
+
+    public static implicit operator AnswersRequest((int challengeId, List<ContestantAnswer> answers) data)
+    {
+        return new AnswersRequest()
+        {
+            ChallengeId = data.challengeId,
+            Answers = data.answers
+        };
+    }
 }
