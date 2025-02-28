88a1176b08a53adee602bdceb59515d12f554241
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 14:46:33 -0300

    feat: criado construtor implicito de ResponseMessage.cs

--- IdentificandoCha/DTOs/ResponseMessage.cs
+++ IdentificandoCha/DTOs/ResponseMessage.cs
@@ -3,4 +3,9 @@
 public class ResponseMessage(string message)
 {
     public string Message { get; } = message;
+
+    public static implicit operator ResponseMessage(string message)
+    {
+        return new ResponseMessage(message);
+    }
 }
