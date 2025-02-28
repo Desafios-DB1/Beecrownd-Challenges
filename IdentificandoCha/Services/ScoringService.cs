618177385f64c9065f7e9df4800f90f508325ce2
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:12:31 -0300

    feat: criado serviço para lógica de pontuação

---
+++ IdentificandoCha/Services/ScoringService.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentificandoCha.Services;
+
+public class ScoringService (ContestantService contestantService)
+{
+    public static void AddPoints(int contestantId, int points)
+    {
+        ContestantService.AddPoints(contestantId, points);
+    }
+}
