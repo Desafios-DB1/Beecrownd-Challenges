45696c25fbdf232512cf4f38d99ff68daa8b4829
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-02-28 13:05:23 -0300

    feat: exception handler personalizado para Lista de strings ou string única

--- IdentificandoCha/Exceptions/BusinessException.cs
+++ IdentificandoCha/Exceptions/BusinessException.cs
@@ -1,3 +1,24 @@
 ﻿namespace IdentificandoCha.Exceptions;
 
-public class BusinessException(string message) : Exception(message);
+public class BusinessException : Exception
+{
+    private List<string> Errors { get; }
+
+    public BusinessException(List<string> errors)
+        : base("Um ou mais erros de validação aconteceram!")
+    {
+        Errors = errors;
+    }
+
+    public BusinessException(string error)
+
+        : base(error)
+    {
+        Errors = [error];
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, Errors);
+    }
+}
