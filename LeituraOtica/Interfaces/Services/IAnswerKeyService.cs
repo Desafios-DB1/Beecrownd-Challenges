6e0639d0704ddc70a683324846938d621ee8363c
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-06 13:28:11 -0300

    refactor

--- LeituraOtica/Interfaces/Services/IAnswerKeyService.cs
+++ LeituraOtica/Interfaces/Services/IAnswerKeyService.cs
@@ -7,6 +7,5 @@ public interface IAnswerKeyService
 {
     public OperationResult SaveAnswerKey(AnswerKeyDto answerKey);
     public AnswerKeyDto? GetAnswerKey(int answerKeyId);
-    
     public List<AnswerKeyDto>? GetAllAnswerKeys();
 }
