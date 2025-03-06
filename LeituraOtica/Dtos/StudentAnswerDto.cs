89cef0e0a9e9aca686d6ab6aebd1e9e726f5fa46
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-06 10:47:12 -0300

    feat: atributo para as respostas convertidas

--- LeituraOtica/Dtos/StudentAnswerDto.cs
+++ LeituraOtica/Dtos/StudentAnswerDto.cs
@@ -1,17 +1,18 @@
 ﻿namespace LeituraOtica.Dtos;
 
-public class StudentAnswerDto (int studentId, int examId, int answerKeyId, Dictionary<int, char> answers)
+public class StudentAnswerDto (int studentId, int examId, int answerKeyId, List<int[]> answers)
 {
     public int Id { get; set; }
     public int StudentId { get; set; } = studentId;
     public int ExamId { get; set; } = examId;
     public int AnswerKeyId { get; set; } = answerKeyId;
-    public Dictionary<int, char> Answers { get; set; } = answers;
+    public List<int[]> Answers { get; set; } = answers;
+    public Dictionary<int, char>? ConvertedAnswers { get; set; }
     
     public double Grade { get; set; }
 
     public static implicit operator StudentAnswerDto(
-        (int studentId, int examId, int answerKeyId, Dictionary<int, char> answers) input)
+        (int studentId, int examId, int answerKeyId, List<int[]> answers) input)
     {
         return new StudentAnswerDto(input.studentId, input.examId, input.answerKeyId, input.answers);
     }
