40e2d83262fba339c0143c21b6be4ee45eb1278a
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-06 09:08:52 -0300

    refactor: removida validação redundante

--- LeituraOtica/Validators/AnswerKeyDtoValidator.cs
+++ LeituraOtica/Validators/AnswerKeyDtoValidator.cs
@@ -8,11 +8,9 @@ public class AnswerKeyDtoValidator : AbstractValidator<AnswerKeyDto>
     public AnswerKeyDtoValidator()
     {
         RuleFor(x => x.ExamId)
-            .NotNull().WithMessage("O id da prova é obrigatório!")
             .NotEmpty().WithMessage("O id da prova não pode estar vazio!");
         
         RuleFor(x => x.Answers)
-            .NotNull().WithMessage("A lista de respostas é obrigatória!")
             .NotEmpty().WithMessage("A lista de respostas não pode estar vazia!");
     }
 }
