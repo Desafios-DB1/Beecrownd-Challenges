f9f033826e8662485b943405c09954d2fe6cc289
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-06 09:09:13 -0300

    refactor: removida validação redundante

--- LeituraOtica/Validators/ExamDtoValidator.cs
+++ LeituraOtica/Validators/ExamDtoValidator.cs
@@ -8,12 +8,13 @@ public class ExamDtoValidator : AbstractValidator<ExamDto>
     public ExamDtoValidator()
     {
         RuleFor(x => x.SubjectName)
-            .NotNull().WithMessage("O nome da matéria é obrigatório!")
             .NotEmpty().WithMessage("O nome da matéria não pode estar vazio!");
-        
+
+        RuleFor(x => x.ResponsibleName)
+            .NotEmpty().WithMessage("O nome do responsável é obrigatório!");
+
         RuleFor(x => x.Value)
-            .Must(value => !double.IsNaN(value)).WithMessage("O valor da prova é obrigatório!")
-            .GreaterThanOrEqualTo(0).WithMessage("A prova deve valer mais que 0!")
-            .LessThanOrEqualTo(10).WithMessage("A prova deve valer 10 ou menos!");
+            .NotNull().WithMessage("O valor da prova é obrigatório!")
+            .GreaterThanOrEqualTo(0).WithMessage("A prova deve valer mais que 0!");
     }
 }
