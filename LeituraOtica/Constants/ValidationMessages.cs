03d9d8a230522b83187aa555d549ec14832098d6
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-07 15:38:34 -0300

    feat: constante validation messages para retornar mensagens de validação padrão

---
+++ LeituraOtica/Constants/ValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace LeituraOtica.Constants;
+
+public static class ValidationMessages
+{
+    public const string RequiredField = "O campo {0} é obrigatório!";
+    public const string NotExistError = "O {0} não existe!";
+}
