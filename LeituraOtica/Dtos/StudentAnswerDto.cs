447827e2329d9463f452693843ef155450a4e8f6
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-07 15:41:46 -0300

    refactor: removido atributo id, grade e convertedAnswers

--- LeituraOtica/Dtos/StudentAnswerDto.cs
+++ LeituraOtica/Dtos/StudentAnswerDto.cs
@@ -2,14 +2,10 @@
 
 public class StudentAnswerDto (int studentId, int examId, int answerKeyId, List<int[]> answers)
 {
-    public int Id { get; set; }
     public int StudentId { get; } = studentId;
     public int ExamId { get; } = examId;
     public int AnswerKeyId { get; } = answerKeyId;
     public List<int[]> Answers { get; } = answers;
-    public Dictionary<int, char>? ConvertedAnswers { get; set; }
-    
-    public double Grade { get; set; }
 
     public static implicit operator StudentAnswerDto(
         (int studentId, int examId, int answerKeyId, List<int[]> answers) input)
