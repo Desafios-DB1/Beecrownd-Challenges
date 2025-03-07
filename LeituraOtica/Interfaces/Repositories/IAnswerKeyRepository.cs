0c65703177b156656510d1fcb0e9e4c006dc36e7
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-07 08:19:47 -0300

    refactor: nome do parâmetro de save para answerKey

--- LeituraOtica/Interfaces/Repositories/IAnswerKeyRepository.cs
+++ LeituraOtica/Interfaces/Repositories/IAnswerKeyRepository.cs
@@ -4,7 +4,7 @@ namespace LeituraOtica.Interfaces.Repositories;
 
 public interface IAnswerKeyRepository
 {
-    public AnswerKeyDto Save(AnswerKeyDto answers);
+    public AnswerKeyDto Save(AnswerKeyDto answerKey);
     public AnswerKeyDto? GetById(int answerKeyId);
     public List<AnswerKeyDto>? GetByExamId(int examId);
     public List<AnswerKeyDto>? GetAll();
