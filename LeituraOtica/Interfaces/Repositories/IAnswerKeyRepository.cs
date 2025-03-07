22d816c595907c46046a50dddbfefc138fc74c86
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-07 15:58:24 -0300

    feat: criado método exists

--- LeituraOtica/Interfaces/Repositories/IAnswerKeyRepository.cs
+++ LeituraOtica/Interfaces/Repositories/IAnswerKeyRepository.cs
@@ -4,9 +4,10 @@ namespace LeituraOtica.Interfaces.Repositories;
 
 public interface IAnswerKeyRepository
 {
-    public AnswerKeyDto Save(AnswerKeyDto answers);
+    public AnswerKeyDto Save(AnswerKeyDto answerKey);
     public AnswerKeyDto? GetById(int answerKeyId);
     public List<AnswerKeyDto>? GetByExamId(int examId);
     public List<AnswerKeyDto>? GetAll();
     public AnswerKeyDto? GetByExamAndKeyId(int examId, int keyId);
+    bool Exists(int answerKeyId);
 }
