8625ecd8e28b79813581690a145f2a98526c5b42
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-03-11 11:45:29 -0300

    feat: mais testes

--- LeituraOtica.Test/Services/ExamCorrectionServiceTest.cs
+++ LeituraOtica.Test/Services/ExamCorrectionServiceTest.cs
@@ -39,4 +39,31 @@ public class ExamCorrectionServiceTest
         
         Assert.Equal(10, result);
     }
+
+    [Fact]
+    public void Correction_Return0_WhenExamDoesNotExist()
+    {
+        var examId = Guid.NewGuid();
+        var answerKeyId = Guid.NewGuid();
+        var studentAnswers = new StudentAnswerWithGradeDto(examId, answerKeyId, new Dictionary<int, char>());
+        _mockExamService.Setup(x=>x.ExamExists(examId)).Returns(false);
+        
+        var result = _service.Correction(studentAnswers);
+        
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Correction_Return0_WhenAnswerKeyDoesNotExist()
+    {
+        var examId = Guid.NewGuid();
+        var answerKeyId = Guid.NewGuid();
+        var studentAnswers = new StudentAnswerWithGradeDto(examId, answerKeyId, new Dictionary<int, char>());
+        _mockExamService.Setup(x=>x.ExamExists(examId)).Returns(true);
+        _mockAnswerKeyService.Setup(x => x.GetAnswerKeyAnswers(answerKeyId)).Returns((Dictionary<int, char>?)null);
+        
+        var result = _service.Correction(studentAnswers);
+        
+        Assert.Equal(0, result);
+    }
 }
