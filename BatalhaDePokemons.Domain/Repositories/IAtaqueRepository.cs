b7d68fff47d72fa060b92cc589315405aa6a3109
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-02 12:10:14 -0300

    BatalhaDePokemons: initial commit

--- BatalhaDePokemons.Domain/Repositories/IAtaqueRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BatalhaDePokemons.Domain.Enums;
-using BatalhaDePokemons.Domain.Models;
-
-namespace BatalhaDePokemons.Domain.Repositories;
-
-public interface IAtaqueRepository : IRepository<Ataque>
-{
-    Task<ICollection<Ataque>> FindByTipoAsync(Tipo tipo);
-}
--- BatalhaDePokemons.Domain/Repositories/IPokemonAtaqueRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BatalhaDePokemons.Domain.Models;
-
-namespace BatalhaDePokemons.Domain.Repositories;
-
-public interface IPokemonAtaqueRepository
-{
-    public Task<ICollection<Ataque>> GetAtaquesByPokemonIdAsync(Guid pokemonId);
-    public Task AddAsync(PokemonAtaque entity);
-}
