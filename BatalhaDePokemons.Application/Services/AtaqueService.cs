c9f1fca8fce8b24fc4014b8960a89b01164f287b
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-03 11:28:18 -0300

    Revert "Delete BatalhaDePokemons.Application directory"
    This reverts commit 0424b26096c7a77787dec0d74d5899c9ce15f4e6.

---
+++ BatalhaDePokemons.Application/Services/AtaqueService.cs
@@ -0,0 +1,59 @@
+﻿using BatalhaDePokemons.Application.Dtos;
+using BatalhaDePokemons.Application.Dtos.Ataque;
+using BatalhaDePokemons.Application.Interfaces;
+using BatalhaDePokemons.Domain.Enums;
+using BatalhaDePokemons.Domain.Models;
+using BatalhaDePokemons.Domain.Repositories;
+
+namespace BatalhaDePokemons.Application.Services;
+
+public class AtaqueService(IAtaqueRepository repository) : IAtaqueService
+{
+    public async Task<AtaqueResponseDto> CriarAsync(AtaqueCreationDto ataque)
+    {
+        var newAtaque = new Ataque(
+            ataque.Name, ataque.Tipo, ataque.Poder, ataque.Precisao, ataque.PP);
+        await repository.AddAsync(newAtaque);
+        return newAtaque;
+    }
+
+    public async Task<AtaqueResponseDto?> ObterPorIdAsync(Guid id)
+    {
+        var ataque = await repository.FindByIdAsync(id);
+        if (ataque == null) return null;
+        return ataque;
+    }
+
+    public async Task<ICollection<AtaqueResponseDto>> ObterTodosAsync()
+    {
+        var ataques = await repository.FindAllAsync();
+        return ataques.Select(ataque => (AtaqueResponseDto)ataque).ToList();
+    }
+
+    public async Task<ICollection<AtaqueResponseDto>> ObterPorTipoAsync(Tipo tipo)
+    {
+        var ataques = await repository.FindByTipoAsync(tipo);
+        return ataques.Select(ataque => (AtaqueResponseDto)ataque).ToList();
+    }
+
+    public async Task<AtaqueResponseDto?> AtualizarAsync(Guid ataqueId, AtaqueCreationDto ataque)
+    {
+        var updatedAtaque = await repository.FindByIdAsync(ataqueId);
+        if (updatedAtaque == null) return null;
+        updatedAtaque.Name = ataque.Name;
+        updatedAtaque.Tipo = ataque.Tipo;
+        updatedAtaque.Poder = ataque.Poder;
+        updatedAtaque.Precisao = ataque.Precisao;
+        updatedAtaque.PP = ataque.PP;
+        
+        await repository.UpdateAsync(updatedAtaque);
+        return updatedAtaque;
+    }
+
+    public async Task RemoverAsync(Guid id)
+    {
+        var ataque = await repository.FindByIdAsync(id);
+        if (ataque == null) return;
+        await repository.RemoveAsync(ataque);
+    }
+}
