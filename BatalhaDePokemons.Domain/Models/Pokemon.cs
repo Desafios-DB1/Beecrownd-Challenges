a8b6da2070416976eb43cbd7f9dc8c051858aa85
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-03 11:28:49 -0300

    Revert "Delete BatalhaDePokemons.Domain directory"
    This reverts commit 430311386c4b8108537e0ebb62f418e2f299f95a.

---
+++ BatalhaDePokemons.Domain/Models/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using BatalhaDePokemons.Domain.Enums;
+
+namespace BatalhaDePokemons.Domain.Models;
+
+public class Pokemon
+{
+    public Pokemon()
+    {
+    }
+
+    public Pokemon(string name, int level, Tipo tipo, int hp, int spd, int def, int atk)
+    {
+        PokemonId = Guid.NewGuid();
+        Name = name;
+        Level = level;
+        Tipo = tipo;
+        Status = new StatusDeCombate(hp, spd, def, atk);
+    }
+
+    public Guid PokemonId { get; init; }
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public bool IsDesmaiado { get; set; }
+    public Tipo Tipo { get; set; }
+    public StatusDeCombate Status { get; set; }
+    public ICollection<Ataque> Ataques { get; set; }
+    public ICollection<PokemonAtaque> PokemonAtaques { get; set; } = [];
+}
+
---
+++ BatalhaDePokemons.Domain/Repositories/IAtaqueRepository.cs
@@ -0,0 +1,9 @@
+﻿using BatalhaDePokemons.Domain.Enums;
+using BatalhaDePokemons.Domain.Models;
+
+namespace BatalhaDePokemons.Domain.Repositories;
+
+public interface IAtaqueRepository : IRepository<Ataque>
+{
+    Task<ICollection<Ataque>> FindByTipoAsync(Tipo tipo);
+}
