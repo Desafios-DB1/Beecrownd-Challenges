49ede4e8eaa9f90c5fe286a6e5578d1c360e0a33
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-03 11:28:38 -0300

    Revert "Delete BatalhaDePokemons.Crosscutting directory"
    This reverts commit ff6f5b18eaa79947fa2d0c2075d81978ba2af22d.

---
+++ BatalhaDePokemons.Crosscutting/Constantes/Caracteres.cs
@@ -0,0 +1,8 @@
+﻿namespace BatalhaDePokemons.Crosscutting.Constantes;
+
+public static class Caracteres
+{
+    public const int Cinco = 5;
+    public const int Cinquenta = 50;
+    public const int Duzentos = 200;
+}
