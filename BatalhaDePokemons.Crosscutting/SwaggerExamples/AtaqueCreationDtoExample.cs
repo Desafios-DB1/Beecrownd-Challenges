c68138c9caf8326d3e9282051f5946c85d588558
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-09 10:24:54 -0300

    feat: adiciona examplos para o swagger

---
+++ BatalhaDePokemons.Crosscutting/SwaggerExamples/AtaqueCreationDtoExample.cs
@@ -0,0 +1,20 @@
+﻿using BatalhaDePokemons.Crosscutting.Dtos.Ataque;
+using BatalhaDePokemons.Crosscutting.Enums;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace BatalhaDePokemons.Crosscutting.SwaggerExamples;
+
+public class AtaqueCreationDtoExample : IExamplesProvider<AtaqueCreationDto>
+{
+    public AtaqueCreationDto GetExamples()
+    {
+        return new AtaqueCreationDto
+        {
+            Nome = "Faísca",
+            Tipo = Tipo.Fogo.ToString(),
+            Poder = 15,
+            Precisao = 70,
+            QuantUsos = 10
+        };
+    }
+}
---
+++ BatalhaDePokemons.Crosscutting/SwaggerExamples/PokemonCreationDtoExample.cs
@@ -0,0 +1,23 @@
+﻿using BatalhaDePokemons.Crosscutting.Dtos.Pokemon;
+using BatalhaDePokemons.Crosscutting.Enums;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace BatalhaDePokemons.Crosscutting.SwaggerExamples;
+
+public class PokemonCreationDtoExample : IExamplesProvider<PokemonCreationDto>
+{
+    public PokemonCreationDto GetExamples()
+    {
+        return new PokemonCreationDto
+        {
+            Nome = "Charmander",
+            Level = 30,
+            Tipo = Tipo.Fogo.ToString(),
+            IsDesmaiado = false,
+            Hp = 40,
+            Spd = 20,
+            Atk = 15,
+            Def = 10
+        };
+    }
+}
